fffbf91ca72d5ed4e2f6eb948f81899875c8ef20
EECG1986 <estivencardona@gmail.com>
2024-01-23 21:39:36 -0500

    practica tipos variables

---
+++ # tipos de variables.py
@@ -0,0 +1,29 @@
+# tipos de variables
+string = "str"
+
+boleano = True 
+
+entero = 33
+
+float = 3.1416
+
+resultado_cadena = string + str(boleano) + ", " + str(entero) + ", " + str(float)
+
+# Límites de enteros y flotantes en Python
+# En Python, los enteros tienen precisión ilimitada, mientras que los flotantes tienen limitaciones de precisión.
+# Los enteros en Python pueden crecer arbitrariamente grandes hasta que se agote la memoria.
+# Los flotantes son usualmente representados en Python utilizando 64 bits, y tienen una precisión de hasta 15-17 dígitos.
+
+# Fórmula de la suma de los primeros n números pares: n * (n + 1)
+suma_pares = entero * (entero + 1) 
+
+
+print(resultado_cadena)
+print("suma de los primeros n numeros pares", suma_pares)
+
+
+
+
+
+
+
